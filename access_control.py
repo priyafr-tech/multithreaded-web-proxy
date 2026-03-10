d39eecfaf214c66d2cddd22b858ccef7f1f6080c
priyafr-tech <priyakkcs06@gmail.com>
2026-03-10 11:06:43 +0530

    access_control.py

--- access_control.py
+++ access_control.py
@@ -1,1 +1,17 @@
+"""
+access_control.py
+Handles blocked website filtering
+"""
 
+def load_blocked_sites(filename="blocked_sites.txt"):
+    try:
+        with open(filename, "r") as file:
+            return [line.strip() for line in file]
+    except FileNotFoundError:
+        return []
+
+def is_blocked(url, blocked_sites):
+    for site in blocked_sites:
+        if site in url:
+            return True
+    return False
