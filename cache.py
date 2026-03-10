012a1e1c74a641cfcb27edea8bdb9c8368b9ab27
priyafr-tech <priyakkcs06@gmail.com>
2026-03-10 11:06:11 +0530

    cache.py

--- cache.py
+++ cache.py
@@ -1,1 +1,24 @@
+"""
+cache.py
+Implements LRU Cache for storing web responses
+"""
 
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity=5):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key):
+        if key not in self.cache:
+            return None
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key, value):
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
